796d3e2289a47946d6faa0023afefa4d17d3d55a
Alex de Vera <adever2@gmail.com>
2024-07-03 22:12:07 -0500

    Initial Commit

---
+++ PokerStats/PokerStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerStatsApp.swift
+//  PokerStats
+//
+//  Created by Alex de Vera on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
