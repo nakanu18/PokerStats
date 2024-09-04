b476feb9623e22e0b7c717cc167c1342107596c9
Alex de Vera <adever2@gmail.com>
2024-09-03 21:42:07 -0500

    SessionsScreen: consolidate SessionCell code

--- PokerStats/Screens/SessionsScreen.swift
+++ PokerStats/Screens/SessionsScreen.swift
@@ -9,21 +9,18 @@ import SwiftUI
 
 struct SessionsScreen: View {
     @EnvironmentObject private var storeModel: StoreModel
-
+    
     @State private var showNewSessionSheet = false
     @State private var isNewSessionCreated = false
-
+    
     private func newSessionView() -> some View {
         Group {
             if let liveSession = storeModel.liveSession {
                 Section(header: Text("Live")) {
-                    Button {
+                    SessionCell(session: liveSession, isLive: true) {
                         storeModel.selectedSession = liveSession
                         NavManager.navigateToSessionDetails(session: liveSession)
-                    } label: {
-                        SessionCell(session: liveSession)
-                    }.foregroundColor(.black)
-                        .listRowBackground(storeModel.liveSession!.isDone ? nil : Color.green)
+                    }
                 }
             }
         }
@@ -32,12 +29,10 @@ struct SessionsScreen: View {
     private func historyView() -> some View {
         Section("History") {
             ForEach(storeModel.sessions) { session in
-                Button {
+                SessionCell(session: session, isLive: false) {
                     storeModel.selectedSession = session
                     NavManager.navigateToSessionDetails(session: session)
-                } label: {
-                    SessionCell(session: session)
-                }.foregroundColor(.white)
+                }
             }.onDelete { offsets in
                 storeModel.sessions.remove(atOffsets: offsets)
             }
@@ -91,40 +86,44 @@ struct SessionsScreen: View {
 #Preview {
     let storeModel = StoreModel.mockEmpty
     @ObservedObject var navManager = NavManager.shared
-
-    // Stack page sheet not working
-    // Navigation not working
+    
+    // TODO: Navigation not working in preview
     return NavigationStack(path: $navManager.path) {
         SessionsScreen()
             .navigationBarTitleDisplayMode(.inline)
     }.preferredColorScheme(.dark)
         .environmentObject(storeModel)
-        
 }
 
 struct SessionCell: View {
     let session: Session
-    
+    var isLive = false
+    var onRowTap: (() -> Void)?
+
     var body: some View {
-        HStack {
-            VStack {
-                HStack {
-                    Text("\(session.id):")
-                        .font(.caption)
-                    Text(session.startDate.shorten())
-                        .font(.caption)
-                    Spacer()
+        Button {
+            onRowTap?()
+        } label: {
+            HStack {
+                VStack {
+                    HStack {
+                        Text("\(session.id):")
+                            .font(.caption)
+                        Text(session.startDate.shorten())
+                            .font(.caption)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("\(session.template.desc)")
+                            .font(.headline)
+                        Spacer()
+                    }
                 }
-                HStack {
-                    Text("\(session.template.desc)")
-                        .font(.headline)
-                    Spacer()
-                }
+                Text(session.profit.toDollars())
+                Image(systemName: "chevron.right")
+                    .foregroundColor(isLive ? .black : .blue)
             }
-            Text(session.profit.toDollars())
-            Spacer()
-            Image(systemName: "chevron.right")
-                .foregroundColor(.blue)
-        }
+        }.foregroundColor(isLive ? .black : .white)
+            .listRowBackground((isLive && !session.isDone) ? Color.green : nil)
     }
 }
